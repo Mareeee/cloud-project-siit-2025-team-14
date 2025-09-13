fc738c485267d6211952c0d87e987e5545dffb76
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2025-09-13 18:05:11 +0200

    [FIX] artists display

--- frontend/src/app/models/song.model.ts
+++ frontend/src/app/models/song.model.ts
@@ -5,4 +5,14 @@ export interface Song {
     albumId: string;
     creationDate: string;
     genres: string[];
+    imageUrl?: string;
+    artistName?: string;
+    artistBio?: string;
+    audioUrl: string;
+    artistNames?: string[];
+    artistBios?: string[];
+    artistGenres?: string[];
+    isPlaying?: boolean;
+    duration?: number;
+    currentTime?: number;
 }
