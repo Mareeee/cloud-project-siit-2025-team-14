08f6ad1e3f2fdfa5ba647993f318d8097cac3170
David Stakic <davidstakic2003@gmail.com>
2025-10-17 00:53:41 +0200

    [ADD] Auth Stack

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from backend_stack.artists_stack import ArtistsStack
 from backend_stack.albums_stack import AlbumsStack
 from backend_stack.songs_stack import SongsStack
 from backend_stack.api_stack import ApiStack
+from backend_stack.auth_stack import AuthStack
 
 app = cdk.App()
 
@@ -14,6 +15,7 @@ genres_stack = GenresStack(app, "GenresStack")
 artists_stack = ArtistsStack(app, "ArtistsStack", genres_table=genres_stack.genres_table)
 albums_stack = AlbumsStack(app, "AlbumsStack", genres_table=genres_stack.genres_table)
 songs_stack = SongsStack(app, "SongsStack", genres_table=genres_stack.genres_table)
+auth_stack = AuthStack(app, "AuthStack")
 
 ApiStack(app, "ApiStack",
           songs_stack=songs_stack,
