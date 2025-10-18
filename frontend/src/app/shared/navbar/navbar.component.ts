eacc69c1475b24d40485791c895b6947af227123
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2025-10-18 19:22:24 +0200

    [FIX] navigation bar

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AuthRoleService, AppRole } from '../auth-role.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  role$: Observable<AppRole>;
-
-  constructor(private auth: AuthRoleService) {
-    this.role$ = this.auth.role$;
-  }
-
-  devSwitch(role: AppRole) {
-    this.auth.setRole(role);
-  }
-}
+export class NavbarComponent { }
