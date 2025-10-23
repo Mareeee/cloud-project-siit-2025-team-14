e1a792a021fbbdee385aa03c3b4e27b826285dd7
David Stakic <davidstakic2003@gmail.com>
2025-10-23 13:11:26 +0200

    Refactored transcription

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,6 @@ from backend_stack.api_stack import ApiStack
 from backend_stack.auth_stack import AuthStack
 from backend_stack.ratings_stack import RatingsStack
 from backend_stack.subscriptions_stack import SubscriptionsStack
-from backend_stack.transcription_stack import TranscriptionStack
 from backend_stack.seeder_stack import SeederStack
 from backend_stack.genre_catalog_stack import GenreCatalogStack
 from backend_stack.notifications_stack import NotificationsStack
@@ -22,12 +21,7 @@ genres_stack = GenresStack(app, "GenresStack")
 genre_catalog_stack = GenreCatalogStack(app, "GenreCatalogStack")
 auth_stack = AuthStack(app, "AuthStack")
 ratings_stack = RatingsStack(app, "RatingsStack", topic=notifications_stack.feed_topic)
-
-transcription_stack = TranscriptionStack(app, "TranscriptionStack")
 songs_stack = SongsStack(app, "SongsStack", genres_table=genres_stack.genres_table, genre_catalog_table=genre_catalog_stack.genre_catalog_table, ratings_table=ratings_stack.ratings_table, notifications_topic=notifications_stack.notifications_topic, feed_topic=notifications_stack.feed_topic)
-songs_stack.attach_transcription_queue(transcription_stack.transcribe_queue)
-transcription_stack.attach_songs_bucket(songs_stack.media_bucket)
-
 artists_stack = ArtistsStack(app, "ArtistsStack", genres_table=genres_stack.genres_table, genre_catalog_table=genre_catalog_stack.genre_catalog_table, artist_catalog_table=songs_stack.artist_catalog_table, topic=notifications_stack.notifications_topic)
 subscriptions_stack = SubscriptionsStack(app, "SubscriptionsStack", artist_table=artists_stack.artists_table, genre_table=genres_stack.genres_table, feed_topic=notifications_stack.feed_topic, notifications_topic=notifications_stack.notifications_topic)
 albums_stack = AlbumsStack(app, "AlbumsStack", genres_table=genres_stack.genres_table, genre_catalog_table=genre_catalog_stack.genre_catalog_table, topic=notifications_stack.notifications_topic)
@@ -52,7 +46,6 @@ ApiStack(app, "ApiStack",
           albums_stack=albums_stack,
           ratings_stack=ratings_stack,
           subscriptions_stack=subscriptions_stack,
-          transcription_stack=transcription_stack,
           genres_stack=genres_stack,
           genre_catalog_stack=genre_catalog_stack,
           auth_stack=auth_stack,
